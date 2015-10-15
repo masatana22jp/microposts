c95d599a78a4a9d1050ac65c26ea5cf9fa2e3c8d
masatana22jp <masatana23jp@gmail.com>
2015-10-15 11:50:52 +0000

    add edit&update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-   def show # 追加
-   @user = User.find(params[:id])
+  before_action :set_user, only: [:show, :edit, :update]
+  
+  def show # 追加
   end
   
   def new
@@ -16,12 +17,27 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      redirect_to root_path , notice: 'Profile updated'
+    else
+      render 'edit'
+    end
+  end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :zone, :profile,
                                  :password_confirmation)
+  end
+  
+   def set_user
+    @user = User.find(params[:id])
   end
   
 end
