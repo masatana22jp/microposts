a71e6346bae51394b9095dbee42c2099f505e6a0
masatana22jp <masatana23jp@gmail.com>
2015-10-20 10:50:25 +0000

    making errer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,21 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
 
   private
 
